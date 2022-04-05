907d75f373c87e3ae432751c912b3a4494da02a0
william vegas <elkareem123@gmail.com>
2022-04-05 13:51:32 -0400

    binary search done

--- Searching Algorithms/binarySearch.js
+++ Searching Algorithms/binarySearch.js
@@ -1,35 +1,49 @@
 /**
- * Write a function called binarySearch which accepts a sorted
- * array and a value and returns the index at which the value exists.
- * Otherwise return -1
+ *  * Es mucho mas rapido que linear search
+ *  * Se elimina la mitad de los items por iteracion
+ *  * Solo funciona en arrays que esten ordenados (sorted)
+ *
+ *  Ejercicio
+ *
+ *  Escribe una funcion binarySearch que recibe un array ordenado y un elemento
+ *  y devuelve la posicion del elemento en el array. Caso contrario devuelve -1
+ *
  */
 
-// binarySearch([1,2,3,4,5,6], 3) = 2
+function binarySearch(array, element) {
+  let leftIndex = 0;
+  let rightIndex = array.length - 1;
+  let middleIndex = Math.floor((rightIndex - leftIndex) / 2);
+  let comparisonValue = array[middleIndex];
 
-function binarySearch(list, number) {
-  if (list.length === 0) {
-    return list[0] === number ? 0 : -1;
+  // Condiciones de salida iniciales
+  if (element > array[array.length - 1] || element < array[0]) {
+    return -1;
   }
 
-  let indexToEvaluate = Math.floor(list.length / 2);
-  let left = 0;
-  let right = list.length - 1;
-
-  
-  while (list[indexToEvaluate] !== number && left < right) {
-    if (number < list[indexToEvaluate]) {
-      right = indexToEvaluate - 1;
-      indexToEvaluate = Math.floor((left + right) / 2);
-    }
-    if (number > list[indexToEvaluate]) {
-      left = indexToEvaluate + 1;
-      indexToEvaluate = Math.floor((left + right) / 2);
+  while (leftIndex <= rightIndex) {
+    if (element === comparisonValue) {
+      return middleIndex;
     }
 
-    console.log(left, indexToEvaluate, right);
+    if (element < comparisonValue) {
+      rightIndex = middleIndex - 1;
+      middleIndex = rightIndex - Math.floor((rightIndex - leftIndex) / 2);
+    }
+
+    if (element > comparisonValue) {
+      leftIndex = middleIndex + 1;
+      middleIndex = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+    }
+    comparisonValue = array[middleIndex];
   }
 
-  return list[indexToEvaluate] === number ? number : -1;
+  return -1;
 }
 
-console.log("resultado", binarySearch([11, 22, 33, 44, 55, 66, 77], 11));
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103));
+
+// [1, 2, 3, 4, 5]
+//  l     c     r -> l = 0; r = 4; center = (r-l)/2 = (4-0)/2 = 2
+//          lc  r -> l = center + 1 = 2 + 1 = 3; r = 4; center = l + (r-l)/2 = (4-3)/2 + 3 = 3
+//       rc  l    -> l = 3; r = center - 1 = 3 - 1 = 2; center = r - (r-l)/2 = 2 - (2-3)/2 = 2 - 0 = 2
