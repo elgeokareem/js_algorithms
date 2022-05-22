4c20c9ebb726a5872154756d6897d7df12b500de
william vegas <elkareem123@gmail.com>
2022-05-22 03:33:14 -0400

    fix in selection sort

--- sorting/selection.js
+++ sorting/selection.js
@@ -1,14 +1,14 @@
 function selectionSort(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    var lowest = i;
-    for (var j = i + 1; j < arr.length; j++) {
+  for (const i = 0; i < arr.length; i++) {
+    const lowest = i;
+    for (const j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[lowest]) {
         lowest = j;
       }
     }
     if (i !== lowest) {
       //SWAP!
-      var temp = arr[i];
+      const temp = arr[i];
       arr[i] = arr[lowest];
       arr[lowest] = temp;
     }
